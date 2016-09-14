99d8afe06a86cb26736121054b5b0248d72a535e
sphassan <sphassan@sbcglobal.net>
2016-09-14 13:50:03 -0500

    Add files via upload

--- Lab-3/Lab3.java
+++ Lab-3/Lab3.java
@@ -1,1 +1,28 @@
-//lmao
+import java.io.*;
+import java.util.*;
+
+public class Lab3
+{
+	public static void main(String args[])
+	{
+		Scanner in = null;
+		try
+		{
+		    in = new Scanner(new File("dates.txt"));
+		}
+		catch (FileNotFoundException exception)
+		{
+		    System.err.println("Failed to open dates.txt");
+		    System.exit(1);
+		}
+		while (in.hasNextLine())
+		{
+		    String line = in.nextLine();
+		    Date date = new Date(line);
+		    System.out.println(date);
+		    // need more code for DateRange objects
+		}
+		
+		in.close();
+	}
+}
